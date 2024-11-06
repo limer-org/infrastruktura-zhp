faec5c2406e56787abbb5ccb939dab0d967b1fce
Paweł Kowalczyk <60511960+kowalczyk-p@users.noreply.github.com>
2024-11-06 08:44:02 +0100

    Delegate port.wegorzewo and bryza.olsztyn (#339)

--- domains/zhp.pl.d/wm.js
+++ domains/zhp.pl.d/wm.js
@@ -1,5 +1,16 @@
 var hostingWm = ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.'];
 
+function Delegate_A_and_AAAA_for_WM (domain) {
+    return [
+        A(domain, '104.22.60.171'),
+        A(domain, '104.22.61.171'),
+        A(domain, '172.67.27.221'),
+        AAAA(domain, '2606:4700:10::ac43:1bdd'),
+        AAAA(domain, '2606:4700:10::6816:3cab'),
+        AAAA(domain, '2606:4700:10::6816:3dab'),
+    ]
+}
+
 D_EXTEND('zhp.pl',
     // Chorągiew
     Delegation_NS('wm', hostingWm),
@@ -25,6 +36,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('olecko', hostingWm),
     Delegation_A_WithCfProxy('olsztyn', '195.78.67.34'), // MS365-44451
     A('ftp.olsztyn', '195.78.67.34'),
+    Delegate_A_and_AAAA_for_WM('bryza.olsztyn'),
     Delegation_NS('orneta', ['ns11.linuxpl.com.', 'dns11.linuxpl.com.']),
     Delegation_A('ostroda', '195.78.67.34'),
     A('ftp.ostroda', '195.78.67.34'),
@@ -39,6 +51,9 @@ D_EXTEND('zhp.pl',
     Delegation_A_WithCfProxy('baldy', '195.78.67.34'), // MS365-44426
     A('ftp.baldy', '195.78.67.34'),
     Delegation_NS('perkoz', hostingWm),
+
+    // port wegorzewo
+    Delegate_A_and_AAAA_for_WM('port.wegorzewo'),
 
     Delegation_A_WithCfProxy('grunwald', '51.75.54.175'),
     Ms365_Subdomain('grunwald','zhp.pl')
