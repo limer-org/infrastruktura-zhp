3faaa20097c53d68315018a2d1c11ff46562ac22
Paweł Kowalczyk <60511960+kowalczyk-p@users.noreply.github.com>
2024-11-07 13:00:59 +0100

    Ms365-48987+ms365-48988 - poprawka (#341)
    * Delegate port.wegorzewo and bryza.olsztyn
    
    * Fix wrong adresses from jira issue
    
    * Remove redundant code

--- domains/zhp.pl.d/wm.js
+++ domains/zhp.pl.d/wm.js
@@ -1,16 +1,5 @@
 var hostingWm = ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.'];
 
-function Delegate_A_and_AAAA_for_WM (domain) {
-    return [
-        A(domain, '104.22.60.171'),
-        A(domain, '104.22.61.171'),
-        A(domain, '172.67.27.221'),
-        AAAA(domain, '2606:4700:10::ac43:1bdd'),
-        AAAA(domain, '2606:4700:10::6816:3cab'),
-        AAAA(domain, '2606:4700:10::6816:3dab'),
-    ]
-}
-
 D_EXTEND('zhp.pl',
     // Chorągiew
     Delegation_NS('wm', hostingWm),
@@ -36,7 +25,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('olecko', hostingWm),
     Delegation_A_WithCfProxy('olsztyn', '195.78.67.34'), // MS365-44451
     A('ftp.olsztyn', '195.78.67.34'),
-    Delegate_A_and_AAAA_for_WM('bryza.olsztyn'),
+    A('bryza.olsztyn', '195.78.67.34'),
     Delegation_NS('orneta', ['ns11.linuxpl.com.', 'dns11.linuxpl.com.']),
     Delegation_A('ostroda', '195.78.67.34'),
     A('ftp.ostroda', '195.78.67.34'),
@@ -53,7 +42,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('perkoz', hostingWm),
 
     // port wegorzewo
-    Delegate_A_and_AAAA_for_WM('port.wegorzewo'),
+    A('port.wegorzewo', '195.78.67.34'),
 
     Delegation_A_WithCfProxy('grunwald', '51.75.54.175'),
     Ms365_Subdomain('grunwald','zhp.pl')
